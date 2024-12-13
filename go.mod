64e86abef4e9020bacf421c7afff98429da83346
Scott Stensland <scottstensland@gmail.com>
2024-12-12 20:46:24 -0500

    working edition primary push

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/scottstensland/turmeric
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,16 @@
+// main.go
+package main
+
+import (
+	"syscall/js"
+)
+
+func add(this js.Value, args []js.Value) interface{} {
+	a, b := args[0].Int(), args[1].Int()
+	return a + b
+}
+
+func main() {
+	js.Global().Set("add", js.FuncOf(add))
+	select {} // Keep the program running
+}
